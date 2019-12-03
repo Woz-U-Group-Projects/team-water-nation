b6e112ddceee129f163e0fcb96c5d019d0621c99
tyszkatech <ian@tyszkatech.com>
2019-12-02 23:19:40 -0500

    Update to React Client App.js

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -2,12 +2,37 @@ import React from "react";
 import Project from "./components/Project";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Project />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'Please write an essay about your favorite DOM element.'
+    };
+
+this.handleChange = this.handleChange.bind(this);
+this.handleSubmit = this.handleSubmit.bind(this);
+}
+};
+
+handleChange(event) {
+this.setState({value: event.target.value});
+};
+
+handleSubmit(event) {
+alert('An essay was submitted: ' + this.state.value);
+event.preventDefault();
+};
+
+render() {
+return (
+<form onSubmit={this.handleSubmit}>
+<label>
+  Essay:
+  <textarea value={this.state.value} onChange={this.handleChange} />
+</label>
+<input type="submit" value="Submit" />
+</form>
+);
 }
 
 export default App;
