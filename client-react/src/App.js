6648ca4df8c4594a877155a660f1d2274f9a8357
AshleyDingler <ashleydingler833@gmail.com>
2019-12-07 19:20:01 -0600

    Set up the DB Fields on STS

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from 'react-dom';
 import Project from "./components/Project";
 import "./App.css";
 
@@ -13,30 +12,29 @@ class App extends React.Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-  
+
   handleChange(event) {
     this.setState({ value: event.target.value });
   }
-  
+
   handleSubmit(event) {
     alert('An essay was submitted: ' + this.state.value);
     event.preventDefault();
   }
-  
+
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
           Essay:
-    <textarea value={this.state.value} onChange={this.handleChange} />
+         <textarea value={this.state.value} onChange={this.handleChange} />
         </label>
         <input type="submit" value="Submit" />
       </form>
     )
-}
+  }
 
 
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
 export default App;
